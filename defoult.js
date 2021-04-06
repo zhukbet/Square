f226f5ec58fb9e560dca7cb136a0a9af22d50b78
zhukbet <ksenzhukd@gmail.com>
2021-04-06 11:18:57 +0300

    add_file cup and defoult

---
+++ defoult.js
@@ -0,0 +1,20 @@
+const P = {
+    W: 'W',
+    H: 'H'
+}
+
+
+const getRandom=(min=0, max=100)=>Math.floor(min +  Math.random() * (max - min+1) )
+
+
+
+let counter=0 
+const id=()=> ++counter
+
+
+
+const allimg=document.querySelector(".all_img")
+const mainbutton=document.querySelector(".main")
+const Obj_ = [
+   
+]
